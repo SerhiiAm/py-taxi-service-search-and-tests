09ad583f0169f68f3e3cace10056074f8db6cb9f
Serhii <sergei.amvarcumov@gmail.com>
2026-07-02 22:11:59 +0200

    Change ManufacturerSearchForm from ModelForm to Form

--- taxi/forms.py
+++ taxi/forms.py
@@ -52,7 +52,7 @@ def validate_license_number(
     return license_number
 
 
-class ManufacturerSearchForm(forms.ModelForm):
+class ManufacturerSearchForm(forms.Form):
     name = forms.CharField(
         max_length=255,
         required=False,
